d65240cf46928caaf984c88cf475e195541fdbd5
wadmahdi <mohamedmahdiinfo@gmail.com>
2025-01-27 20:57:20 +0200

    add Multi User Admin Login System using Laravel Jetstream

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+
+Route::get('/home' , [AdminController::class, 'index'])->name('home');
