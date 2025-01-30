b679b4c08989fd3be594cea44469599a756b7475
wadmahdi <mohamedmahdiinfo@gmail.com>
2025-01-30 19:30:16 +0200

    Adding Logout option in Admin Dashboard

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -81,9 +81,7 @@
 
                         <x-slot name="content">
                             <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
+                           
 
                             <x-dropdown-link href="{{ route('profile.show') }}">
                                 {{ __('Profile') }}
@@ -95,7 +93,6 @@
                                 </x-dropdown-link>
                             @endif
 
-                            <div class="border-t border-gray-200"></div>
 
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}" x-data>
