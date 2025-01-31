7f60df2d35eb3ad23aed3efcf12961b6d7e381f7
wadmahdi <mohamedmahdiinfo@gmail.com>
2025-01-31 14:02:02 +0200

    Adding قخخة Data from in Admin Panel

---
+++ resources/views/admin/create_room.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    @include('admin.css')
+
+    <style>
+        label {
+
+            display: inline-block;
+            width: 200px;
+        }
+
+        .div_dig {
+
+            padding-top: 30px;
+        }
+
+        .div_center {
+
+            text-align: center;
+            padding-top: 30px;
+
+
+        }
+    </style>
+</head>
+
+<body>
+    @include('admin.header')
+    @include('admin.sidebar')
+
+
+    <div class="page-content">
+        <div class="page-header">
+            <div class="container-fluid">
+
+                <div class="div_center">
+                    <h1 style="font-size: 40px ; font-weight: bold;">Add Rooms</h1>
+
+                    <form action="{{ url('add_room') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="div_dig">
+                            <label>Room Tile</label>
+                            <input type="text" name="title">
+                        </div>
+
+                        <div class="div_dig">
+                            <label>Description</label>
+                            <textarea name="description"></textarea>
+                        </div>
+
+                        <div class="div_dig">
+                            <label>price</label>
+                            <input type="text" name="price">
+                        </div>
+
+                        <div class="div_dig">
+                            <label>Room Type</label>
+                            <select name="type">
+                                <option selected value="regular">Regular</option>
+                                <option value="premium">Premium</option>
+                                <option value="deluxe">Deluxe</option>
+                            </select>
+                        </div>
+
+                        <div class="div_dig">
+                            <label>Free WiFi</label>
+                            <select name="wifi">
+                                <option selected value="yes">yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+
+                        <div class="div_dig">
+                            <label>Upload Image</label>
+                            <input type="file" name="image">
+                        </div>
+
+                        <div class="div_dig">
+                            <input class="btn btn-primary" type="submit" value="Add Room">
+                        </div>
+                    </form>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+
+
+    @include('admin.footer')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/' , [AdminController::class, 'home']);
 
 Route::get('/home' , [AdminController::class, 'index'])->name('home');
+Route::get('/create_room' , [AdminController::class, 'create_room']);
+Route::post('/add_room' , [AdminController::class, 'add_room']);
